ada204965ddb8dd18dbd67a335585dd8f75b2581
root <root@DESKTOP-REUB2EU.>
2024-12-29 00:45:29 +0100

    classe Lecteur

--- Lecteur.cpp
+++ Lecteur.cpp
@@ -1,14 +1,43 @@
+#include "Lecteur.h"
 
 
-bool Lecteur::is_password(std::string password) {
-	if (password == _password) {
-		return true;
+Lecteur::Lecteur(int unique_id, std::string first_name, std::string last_name, int day, int month, int year) : _unique_id(unique_id), _first_name(first_name), _last_name(last_name), _birthday(day, month, year) {
+
+}
+
+std::string Lecteur::get_first_name() const {
+	return _first_name;
+}
+
+std::string Lecteur::get_last_name() const {
+	return _last_name;
+}
+
+void Lecteur::display_birthday() const {
+	_birthday.display_date();
+}
+
+void Lecteur::display_ISBN_list() const {
+	std::cout << "Les ISBN des livres empruntés par cet utilisateur sont: " << std::endl;
+	for (size_t i = 0; i < _ISBNs.size(); i++) {
+		std::cout << _ISBNs[i] << std::endl;
 	}
-	else return false;
 }
 
-Lecteur(std::string unique_id, std::string first_name, std::string last_name, std::string password, int day, int month, int year) : _unique_id(unique_id), _first_name(first_name), _last_name(last_name), _password(password), _birthday(day, month, year) {
-
+void Lecteur::add_ISBN(int ISBN) {
+	_ISBNs.push_back(ISBN);
 }
 
-
+int main() {
+	Lecteur anda(12, "cyrille", "anda", 31, 03, 2006);
+	std::cout << anda.get_first_name();
+	std::cout << anda.get_last_name();
+	anda.display_birthday();
+	anda.display_ISBN_list();
+	anda.add_ISBN(23);
+	anda.add_ISBN(24);
+	anda.add_ISBN(25);
+	anda.add_ISBN(26);
+	anda.display_ISBN_list();
+	return 0;
+}
--- Lecteur.h
+++ Lecteur.h
@@ -1,23 +1,20 @@
-
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include "date.h"
 
 class Lecteur {
-
 private:
-	std::string _unique_id, _first_name, _last_name, _password;
+	int _unique_id;
+	std::string _first_name, _last_name;
 	Date _birthday;
-	std::vector<Livre> _borrowed_book;
-	bool is_password(std::string password);
+	std::vector<int> _ISBNs;
 
 public: 
-	Lecteur(std::string unique_id, std::string first_name, std::string last_name, std::string password, int day, int month, int year); 
-	
-
-	std::string get_personal_info(password) const;
-
-	std::string loan_a_book(int _ISBN); // parmi la base de donnees des livres	aller chercher le livre donc l'id correspond et l'enregistrer dans la structure vector, si je peux je vais une fois modifier ici la liste des emprunteurs associés au livre dont on parle
-	// dans cette methode, je dois verifier si le livre est disponible dans la bibliotheque, je dois donc verifier l'etat de chaque livre
-	// je dois etre capable ici d'extraire des elements de la base de donnees des livres et les renvoyer dans la table associée au lecteur 
-
-
-
+	Lecteur(int unique_id, std::string first_name, std::string last_name, int day, int month, int year); 
+	std::string get_first_name() const;
+	std::string get_last_name() const;
+	void display_birthday() const;
+	void display_ISBN_list() const;
+	void add_ISBN(int ISBN);
 };
