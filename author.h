97850f42ca0115423916e8546b6f454bbb5de552
root <root@DESKTOP-REUB2EU.>
2024-12-29 16:36:23 +0100

    livre

---
+++ author.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include "date.h"
+
+
+class Author {
+private:
+    std::string _full_name;
+    Date _birthday;
+    int _author_id;
+
+public:
+    Author(std::string full_name, int day, int monht, int year, int author_id);
+    std::string get_full_name() const;
+    int get_author_id() const;
+    void display_info_author() const;
+};
